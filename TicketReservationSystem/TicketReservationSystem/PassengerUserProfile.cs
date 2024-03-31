54ca576952d8dfbcee3abe94a65d5d2f446b5def
Asif Ahmad <asif141201@gmail.com>
2024-04-01 01:37:26 +0600

    Add files via upload

---
+++ TicketReservationSystem/TicketReservationSystem/PassengerUserProfile.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace TicketReservationSystem
+{
+    public partial class PassengerUserProfile : UserControl
+    {
+        public PassengerUserProfile()
+        {
+            InitializeComponent();
+        }
+        SqlConnection con = new SqlConnection("Data Source=SAKIBS-ZENBOOK\\SQLEXPRESS;Initial Catalog=EasyTrip;Integrated Security=True");
+
+        private void profileUpdate_Click(object sender, EventArgs e)
+        {
+            profileSave.Visible = true;
+            profileUpdate.Visible = false;
+
+            User_firstName.ReadOnly = false;
+            User_lastName.ReadOnly = false;
+            User_email.ReadOnly = false;
+            User_phn.ReadOnly = false;
+            userPassword.ReadOnly = false;
+        }
+
+        private void profileSave_Click(object sender, EventArgs e)
+        {
+            profileUpdate.Visible = true;
+            profileSave.Visible = false;
+
+            User_firstName.ReadOnly = true;
+            User_lastName.ReadOnly = true;
+            User_email.ReadOnly = true;
+            User_phn.ReadOnly = true;
+            userPassword.ReadOnly = true;
+
+            string query1 = "update  userInfo set Password='" + userPassword.Text + "'";
+
+            SqlCommand cmd = new SqlCommand(query1, con);
+
+            con.Open();
+
+            if (con.State == ConnectionState.Open)
+            {
+                cmd.ExecuteNonQuery();
+
+                MessageBox.Show("Update is Successful");
+            }
+            else
+            {
+                MessageBox.Show("Error");
+            }
+
+            con.Close();
+        }
+
+        private void userShowPass_CheckedChanged(object sender, EventArgs e)
+        {
+            if (userShowPass.Checked == true)
+            {
+                userPassword.UseSystemPasswordChar = false;
+            }
+            else
+            {
+                userPassword.UseSystemPasswordChar = true;
+            }
+        }
+
+        private void PassengerUserProfile_Load(object sender, EventArgs e)
+        {
+            User_firstName.Text = LoggedInUserDetails.userfirstName;
+            User_lastName.Text = LoggedInUserDetails.userlastName;
+            User_userName.Text = LoggedInUserDetails.userName;
+            User_email.Text = LoggedInUserDetails.userEmail;
+            User_phn.Text = LoggedInUserDetails.userPhoneNo;
+            user_dob.Text = LoggedInUserDetails.userDOB;
+            user_gender.Text = LoggedInUserDetails.userGender;
+            userPassword.Text = LoggedInUserDetails.userPassword;
+        }
+
+      
+    }
+}
